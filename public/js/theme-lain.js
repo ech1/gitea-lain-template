c816718dcccde6740bb3905a9d3b7a8083c9bb42
ech1 <ech1@nowhere.com>
2020-11-08 15:52:33 +0100

    89864C

--- public/js/theme-lain.js
+++ public/js/theme-lain.js
@@ -34,7 +34,7 @@ function draw() {
     ctx.fillStyle = "rgba(41, 22, 28, 0.04)";
     ctx.fillRect(0, 0, c.width, c.height);
 
-    ctx.fillStyle = "#D0AAB5"; // Gitea red
+    ctx.fillStyle = "#89864C"; // Gitea red
     //ctx.fillStyle = "red";
     ctx.font = font_size + "px arial";
     //looping over drops
